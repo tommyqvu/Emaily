339a5d1b6005b7f3e307d6b113523619339fd100
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-08-13 22:24:30 +0200

    Refactor

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  OAUTH_CLIENT_ID:
+   process.env.OAUTH_CLIENT_ID,
+  OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+  MONGODB_ATLAS:
+  process.env.MONGODB_ATLAS,
+  cookieKey: process.env.cookieKey,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  STRIPE_PUB_KEY: process.env.STRIPE_PUB_KEY,
+};
